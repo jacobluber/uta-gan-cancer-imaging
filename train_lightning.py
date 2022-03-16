f39d32d606dfe3958263de5583c2e45a0004596a
mxs2361 <mxs2361@cse-133725.uta.edu>
2022-03-16 12:08:11 -0500

    add __main__

--- train_lightning.py
+++ train_lightning.py
@@ -6,16 +6,21 @@ from pytorch_lightning.loggers import TensorBoardLogger
 
 import pytorch_lightning as pl
 
-opt = TrainOption().parse()
-print(opt)
-data = CODEXDataModule(src_data_dir = opt.input_dir_train, \
-tgt_data_dir = opt.target_dir_train, demo_data=False)
-data.prepare_data()
-dataloader = data.train_dataloader()
 
-display_step = 1
 
-pix2pix = Pix2PixHDCodex(opt, display_step=display_step)
-logger = TensorBoardLogger("tb_logs", name="pix2pixcodex")
-trainer = pl.Trainer(max_epochs=1000, gpus=-1, logger = logger)
-trainer.fit(pix2pix, dataloader)
+if __name__ == '__main__':
+    opt = TrainOption().parse()
+    print(opt)
+    data = CODEXDataModule(src_data_dir = opt.input_dir_train, \
+    tgt_data_dir = opt.target_dir_train, demo_data=False)
+    data.prepare_data()
+    dataloader = data.train_dataloader()
+    
+    display_step = 1
+    
+    pix2pix = Pix2PixHDCodex(opt, display_step)
+    logger = TensorBoardLogger("tb_logs", name="lightning_codex")
+    # trainer = pl.Trainer(max_epochs=1000, gpus=-1, logger = logger)
+    trainer = pl.Trainer(max_epochs=1000, gpus = 2, min_epochs=1, auto_lr_find=True, auto_scale_batch_size=False,
+                          progress_bar_refresh_rate=10)
+    trainer.fit(pix2pix, dataloader)
